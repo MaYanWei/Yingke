ca801d67f21a380ed83595c795bcdea00cae2567
mayw <736670509@qq.com>
2016-11-07 13:36:09 +0800

    添加图片

---
+++ Yingke/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  Yingke
+//
+//  Created by mayw on 2016/11/7.
+//  Copyright © 2016年 mayw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ Yingke/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Yingke
+//
+//  Created by mayw on 2016/11/7.
+//  Copyright © 2016年 mayw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
